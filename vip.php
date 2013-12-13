1b15be5ab4307c2118dc162ae8a6456e4cbfdbc5
martinkure <volley4_herrer@yahoo.dk>
2013-12-13 10:04:32 +0100

    friday

--- vip.php
+++ vip.php
@@ -1,7 +1,7 @@
 	<h3 class="vip-headline">Be a VIP member </h3>
 	<p> Receive info about new products and participate in our FREE product contests</p>
 	<article class="col-lg-6">
-		<form role="form" action="welcome.php" method="post" id="formVip">
+		<form role="form" id="formVip">
 		  <div class="form-group">
 			<label for="exampleInputEmail1">Email address</label>
 			<input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
@@ -10,7 +10,7 @@
 			<label for="exampleInputText1">Name</label>
 			<input type="text" name="personName" class="form-control" id="exampleInputText1" placeholder="Enter name">
 		  </div>
-		  <button type="submit" id="newsletterButton" class="btn btn-success newsletter">Submit</button>
+		  <a href="#" id="newsletterButton" class="btn btn-success newsletter">Submit</a>
 		</form>
 	</article>	
 	
